72d89b72b0066329d232b86d19c8f2aba3e40f1e
Januacele <januacelevieira@hotmail.com>
2022-09-08 16:55:45 -0300

    Starting project - configuration, architecture and error handling

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import "express-async-errors";
+import router from "./routes/index";
+import handleErrorsMiddleware from "./middlewares/handleErrorsMiddleware";
+
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+const app = express();
+app.use(json());
+app.use(router);
+app.use(handleErrorsMiddleware);
+
+const port = Number(process.env.PORT) || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
---
+++ src/routes/authRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+const authRouter = Router();
+
+authRouter.post("/signup")
+authRouter.post("/signin")
+
+
+export default authRouter;
