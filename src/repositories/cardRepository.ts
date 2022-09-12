0b31079de11809ecb0a87bf4b9ad4df48d41dfaf
Januacele <januacelevieira@hotmail.com>
2022-09-12 13:57:50 -0300

    Add card repository database consults

---
+++ src/repositories/cardRepository.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../config/database";
+import { ICardData } from "../types/cardTypes";
+
+
+export async function insertCredential(userId: number, card: ICardData){
+    return prisma.card.create({
+        data: {...card, userId}
+    });
+}
+
+export async function getCardByTitle(userId: number, title: string){
+    return prisma.card.findFirst({
+        where: {userId, title}
+    });
+}
+
+export async function getAllCards(userId: number){
+    return prisma.card.findMany({
+        where: {userId}
+    });
+}
+
+export async function getOneCard(userId: number, cardId: number){
+    return prisma.card.findFirst({
+        where: {
+            userId,
+            id: cardId
+        }
+    });
+}
+
+export async function deleteCard(id: number){
+    return prisma.card.delete({
+        where: {id}
+    });
+}
