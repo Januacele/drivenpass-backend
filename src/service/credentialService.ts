5353de491085ef86bcd66d39553946ee9dffcc9c
Januacele <januacelevieira@hotmail.com>
2022-09-12 15:15:24 -0300

    adjust credential service

--- src/service/credentialService.ts
+++ src/service/credentialService.ts
@@ -3,7 +3,7 @@ import Cryptr from "cryptr";
 import { ICredentialData } from "../types/credentialTypes";
 import { conflictError, notFoundError } from "../utils/errorUtils";
 import * as credentialRepository from "../repositories/credentialRepository";
-import { decrypt } from "../utils/criptrUtils";
+import { encrypt, decrypt } from "../utils/criptrUtils";
 import "../setup";
 
 
@@ -13,23 +13,23 @@ async function createCredential(user: User, credential: ICredentialData){
     const existingCredential = await credentialRepository.getCredentialTitle(user.id, credential.title);
     if(existingCredential) throw conflictError("Title already exists");
 
-    const credentialPassword = credential.password;
-    const cryptr = new Cryptr(process.env.CRYPT_SECRET!);
-   
-    const encryptedPassword = cryptr.encrypt(credentialPassword);
-    const credentialInfos = {...credential, password: encryptedPassword}
+    const credentialInfos: ICredentialData = {
+        ...credential, 
+        password: encrypt(credential.password),
+      }
 
     await credentialRepository.insertCredential(user.id, credentialInfos)
 }
 
 
-async function getAllCredential(userId: number){
-    const credential = await credentialRepository.getAllCredential(userId);
-    
-    return credential.map(credential => {
-        const { password } = credential;
-        return {...credential, password: decrypt(password)}
-      })
+async function getAllCredential(userId: number) {
+  const credentials = await credentialRepository.getAllCredential(userId);
+  return credentials.map(credential => {
+    return {
+      ...credential, 
+      password: decrypt(credential.password),
+    }
+  })
 }
 
 
